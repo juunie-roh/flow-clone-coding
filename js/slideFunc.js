5444ba25807e374173a89545474521af13509f34
Juun <blacklily7513@gmail.com>
2023-03-31 13:22:25 +0900

    slideTimer update, slide timer reset on click

--- js/slideFunc.js
+++ js/slideFunc.js
@@ -11,7 +11,9 @@ const bgUrlList = [
   "./images/main/main-function5.png",
 ];
 
-function slideTimeHandler() {
+let slideTimer = setInterval(slideTimerHandler, 5000);
+
+function slideTimerHandler() {
   const currentList = document.querySelector("#slideList li.active");
 
   let nextList;
@@ -20,10 +22,10 @@ function slideTimeHandler() {
   } else {
     nextList = currentList.nextElementSibling;
   }
-  
+
   currentList.classList.remove('active');
   nextList.classList.add('active');
-  
+
   if (slideNum1.innerText === '5') {
     slideNum1.innerText = '1';
     slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
@@ -34,39 +36,14 @@ function slideTimeHandler() {
     slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
     slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
   }
-  
+
   containerFunctions.style.backgroundImage = `url(${bgUrlList[parseInt(slideNum1.innerText) - 1]})`;
 }
 
-const slideTimer = setInterval(function() {
-  const currentList = document.querySelector("#slideList li.active");
+slideList.forEach(function (list, index) {
+  list.addEventListener('click', function () {
+    clearInterval(slideTimer);
 
-  let nextList;
-  if (currentList === slideList[slideList.length - 1]) {
-    nextList = slideList[0];
-  } else {
-    nextList = currentList.nextElementSibling;
-  }
-  
-  currentList.classList.remove('active');
-  nextList.classList.add('active');
-  
-  if (slideNum1.innerText === '5') {
-    slideNum1.innerText = '1';
-    slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
-  } else if (slideNum1.innerText === '4') {
-    slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
-    slideNum2.innerText = '1';
-  } else {
-    slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
-    slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
-  }
-  
-  containerFunctions.style.backgroundImage = `url(${bgUrlList[parseInt(slideNum1.innerText) - 1]})`;
-}, 1000);
-
-slideList.forEach(function(list, index) {
-  list.addEventListener('click', function() {
     for (let li of slideList) {
       li.classList.remove('active');
     }
@@ -78,5 +55,7 @@ slideList.forEach(function(list, index) {
     } else {
       slideNum2.innerText = parseInt(index) + 2;
     }
+    
+    slideTimer = setInterval(slideTimerHandler, 5000);
   })
 });

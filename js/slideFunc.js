ad4c0d521a515294f62bcd80b376e9852cfabd35
Juun <blacklily7513@gmail.com>
2023-03-31 13:18:53 +0900

    slideFunc js update(image slide, activate buttons)

--- js/slideFunc.js
+++ js/slideFunc.js
@@ -1,7 +1,7 @@
-const slideList = document.getElementById("slideList").children;
+const slideList = document.querySelectorAll("#slideList li");
 const slideNum1 = document.getElementById("slideNum1");
 const slideNum2 = document.getElementById("slideNum2");
-const functionBg = document.getElementsByClassName("container__functions")[0];
+const containerFunctions = document.querySelector(".container__functions");
 
 const bgUrlList = [
   "./images/main/main-function1.png",
@@ -11,20 +11,72 @@ const bgUrlList = [
   "./images/main/main-function5.png",
 ];
 
-for (let i in slideList) {
-  if (typeof (slideList[i]) === "object") {
-    slideList[i].addEventListener('click', function () {
-      for (let li of slideList) {
-        li.classList.remove('active');
-      }
-      slideList[i].classList.add('active');
-      functionBg.style.backgroundImage = `url(${bgUrlList[i]})`;
-      slideNum1.innerText = parseInt(i) + 1;
-      if (i === '4') {
-        slideNum2.innerText = 1;
-      } else {
-        slideNum2.innerText = parseInt(i) + 2;
-      }
-    })
+function slideTimeHandler() {
+  const currentList = document.querySelector("#slideList li.active");
+
+  let nextList;
+  if (currentList === slideList[slideList.length - 1]) {
+    nextList = slideList[0];
+  } else {
+    nextList = currentList.nextElementSibling;
   }
+  
+  currentList.classList.remove('active');
+  nextList.classList.add('active');
+  
+  if (slideNum1.innerText === '5') {
+    slideNum1.innerText = '1';
+    slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
+  } else if (slideNum1.innerText === '4') {
+    slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
+    slideNum2.innerText = '1';
+  } else {
+    slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
+    slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
+  }
+  
+  containerFunctions.style.backgroundImage = `url(${bgUrlList[parseInt(slideNum1.innerText) - 1]})`;
 }
+
+const slideTimer = setInterval(function() {
+  const currentList = document.querySelector("#slideList li.active");
+
+  let nextList;
+  if (currentList === slideList[slideList.length - 1]) {
+    nextList = slideList[0];
+  } else {
+    nextList = currentList.nextElementSibling;
+  }
+  
+  currentList.classList.remove('active');
+  nextList.classList.add('active');
+  
+  if (slideNum1.innerText === '5') {
+    slideNum1.innerText = '1';
+    slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
+  } else if (slideNum1.innerText === '4') {
+    slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
+    slideNum2.innerText = '1';
+  } else {
+    slideNum1.innerText = parseInt(slideNum1.innerText) + 1;
+    slideNum2.innerText = parseInt(slideNum2.innerText) + 1;
+  }
+  
+  containerFunctions.style.backgroundImage = `url(${bgUrlList[parseInt(slideNum1.innerText) - 1]})`;
+}, 1000);
+
+slideList.forEach(function(list, index) {
+  list.addEventListener('click', function() {
+    for (let li of slideList) {
+      li.classList.remove('active');
+    }
+    list.classList.add('active');
+    containerFunctions.style.backgroundImage = `url(${bgUrlList[index]})`;
+    slideNum1.innerText = parseInt(index) + 1;
+    if (index === 4) {
+      slideNum2.innerText = 1;
+    } else {
+      slideNum2.innerText = parseInt(index) + 2;
+    }
+  })
+});

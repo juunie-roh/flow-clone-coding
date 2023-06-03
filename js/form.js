262fa17ae3b4c39a8914b2c8a15f67442ab1e22a
Juun <blacklily7513@gmail.com>
2023-06-03 13:05:17 +0900

    input validation added

--- js/form.js
+++ js/form.js
@@ -7,35 +7,53 @@ const selects = document.querySelectorAll('select');
 
 const formDatas = new Array();
 
+let privacyChecked = false;
+let marketingChecked = false;
+
 privacyAgreeLabel.addEventListener('click', function() {
   privacyAgreeLabel.classList.toggle('checked');
   privacyAgree.checked = !privacyAgree.checked;
+  privacyChecked = privacyAgree.checked;
 });
 
 marketingAgreeLabel.addEventListener('click', function() {
   marketingAgreeLabel.classList.toggle('checked');
   marketingAgree.checked = !marketingAgree.checked;
+  marketingChecked = marketingAgree.checked;
 });
-
 
 const submitBtn = document.getElementById('submitBtn');
 submitBtn.addEventListener('click', function(event) {
   event.preventDefault();
 
   const data = new FormData();
+
   for (let input of inputs) {
     if (input.type === 'checkbox' || input.type === 'submit') continue;
+    // Validation of inputs
+    if ( !input.value ) { alert( `${input.placeholder}을(를) 입력해주세요.`); return; }
     data.append(input.id, input.value);
   }
 
   for (let select of selects) {
+    // Validation of select inputs
+    if ( select.value === "" ) {
+      alert( `${select.querySelector( 'option' ).innerText}` );
+      return;
+    }
     data.append(select.id, select.value);
   }
 
-  // if (!privacyAgree.checked) {
-  //   alert("개인정보 활용에 동의를 하셔야합니다.")
-  //   return;
-  // }
+  // Validation
+  if ( !privacyChecked ) {
+    alert("개인정보 활용에 동의를 하셔야합니다.")
+    return;
+  }
+
+  if ( !marketingChecked ) {
+    alert( "마케팅 수집 이용에 동의하셔야 합니다." );
+    return;
+  }
 
   for (let key of data.keys()) {
     console.log(key, ": ", data.get(key));
@@ -43,4 +61,5 @@ submitBtn.addEventListener('click', function(event) {
 
   formDatas.push(data);
   console.log(formDatas);
+  
 });
